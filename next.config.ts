e206b772202a4031e6779ee44f1261ad5e9f770b
MalikaRakhimova31 <malikarakhimova31@gmail.com>
2025-08-22 19:17:03 +0500

    feat: config fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // enables static export for GitHub Pages
+  basePath: isProd ? "/tarteeb-landing" : "",
+  assetPrefix: isProd ? "/tarteeb-landing/" : "",
+  images: {
+    unoptimized: true, // disable next/image optimization (needed for static export)
+  },
 };
 
 export default nextConfig;
